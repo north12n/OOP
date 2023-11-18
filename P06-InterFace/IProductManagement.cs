6895c531293ebf19225353907bf0a32e1e4b4764
north12n <952547north@gmail.com>
2023-11-18 09:44:15 +0700

    GroupByPriceGroup

--- P06-InterFace/IProductManagement.cs
+++ P06-InterFace/IProductManagement.cs
@@ -1,5 +1,6 @@
 ﻿namespace P06_Interface
 {
+    //ใส่สิ่งที่จะทำ ตอนตักเตอ
     public interface IProductManagement
     {
         void AddProduct(Product product);
--- P06-InterFace/ProductService.cs
+++ P06-InterFace/ProductService.cs
@@ -1,9 +1,10 @@
-﻿namespace P06_Interface
+﻿using System.Text.RegularExpressions;
+
+namespace P06_Interface
 {
     public class ProductService
     {
         public ProductManagement ProductManagement { get; set; }
-
         public ProductService()
         {
             ProductManagement = new ProductManagement();
@@ -12,24 +13,23 @@
         public void GenerateProduct(int number = 1)
         {
             Random random = new Random();
-
             for (int i = 1; i <= number; i++)
             {
                 ProductManagement.AddProduct(new Product
                 {
                     Id = i,
-                    Name = "Coffee-" + i,
-                    Price = random.Next(10, 200)
+                    Name = "Coffee",
+                    Price = random.Next(10, 200),
+                    PriceGr = random.Next(1, 5)
                 });
             }
-
         }
 
         public void DisplayProduct()
         {
             foreach (var item in ProductManagement.GetProducts())
             {
-                Console.WriteLine($"{item.Id,5} {item.Name,-10} {item.Price,5}");
+                Console.WriteLine($" {item.Id,5}  {item.Name,-7} {item.Price,4} {item.PriceGr,4}");
             }
         }
 
@@ -42,7 +42,7 @@
         public List<TempGroup> GroupByPrice()
         {
             var temp = new List<TempGroup>();
-            int tempGr;
+            int tempGr, tempty;
 
             foreach (var item in OrderByPrice())
             {
@@ -53,20 +53,27 @@
                     _ => 3,
                 };
 
-                temp.Add(new TempGroup { group = tempGr });
-
+                tempty = item.PriceGr;
+                temp.Add(new TempGroup { group = tempGr, groupTy = tempty });
             }
             return temp;
         }
 
-        public void DisplayGroupByPrice()
+        public void DisplayGroupByPrice()   
         {
-            var numOfGroup = GroupByPrice().GroupBy(p => p.group);
-
+            var numOfGroup = GroupByPrice().GroupBy(g => g.group);
+      
+            Console.WriteLine(" Product Type \t1\t2\t3\t4\t5");
+            Console.WriteLine("===================================================");
             foreach (var item in numOfGroup)
             {
-                Console.WriteLine($"{NameOfPrice(item.Key),8} " +
-                    $"{GroupByPrice().Count(p => p.group == item.Key)}");
+                Console.Write($" {NameOfPrice(item.Key)} ");
+                for (int i = 0; i <= 4; i++)
+                {
+                    //Console.Write($"{item.Count(p => p.groupTy == i),7} ");
+                    Console.Write($"{item.Count(p => p.groupTy.Equals(i)),7} ");
+                }
+                Console.WriteLine(" ");
             }
         }
 
@@ -74,18 +81,16 @@
         {
             return price switch
             {
-                1 => "10-100",
-                2 => "101-200",
+                1 => "  10-100",
+                2 => " 101-200",
                 _ => " ",
             };
         }
-
     }
 
     public class TempGroup
     {
         public int group { get; set; }
+        public int groupTy { get; set; }
     }
-
-
 }

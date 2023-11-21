6cb6fd19e844af9d101a593da5471d2a82fcec2b
north12n <952547north@gmail.com>
2023-11-21 13:55:15 +0700

    Try Catch

---
+++ P07-Exception/Program.cs
@@ -0,0 +1,33 @@
+﻿
+try
+{
+    var test = int.Parse(Console.ReadLine());
+}
+catch (Exception TsetError)
+{
+    Console.WriteLine(TsetError.Message);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

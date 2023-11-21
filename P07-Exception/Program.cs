54b2c0d138ad32b486dd186d023098b98f7c17ce
north12n <952547north@gmail.com>
2023-11-21 14:28:04 +0700

    Dictionary

--- P07-Exception/Program.cs
+++ P07-Exception/Program.cs
@@ -24,10 +24,3 @@ catch (Exception TsetError)
 
 
 
-
-
-
-
-
-
-
---
+++ P08-Dictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Dictionary<string,string> Country = new Dictionary<string,string>();
+Dictionary<int, Double> UnitOfMoney = new Dictionary<int, Double>();
+
+
+Country.Add("Kan", "Kanchanaburi");
+Country.Add("Cm","ChaiengMai");
+Country.Add("BKK","BangKok");
+
+UnitOfMoney.Add(1, 1_000_000);
+UnitOfMoney.Add(2, 2_000_000);
+UnitOfMoney.Add(3, 3_000_000);
+UnitOfMoney.Add(4, 4_000_000);
+UnitOfMoney.Add(5, 5_000_000);
+
+Console.WriteLine(Country["Kan"]);
+if(Country.ContainsKey("Kan")) Console.WriteLine(Country["Kan"]);
+
+foreach (var item in UnitOfMoney)
+{
+    Console.WriteLine(item.Key);
+    Console.WriteLine($"{item.Key} {item.Value}");
+}
+
+var total = UnitOfMoney[1] + UnitOfMoney[2];
+
+UnitOfMoney.Sum(x => x.Value);
+
+Console.WriteLine(total);
+Console.WriteLine();
